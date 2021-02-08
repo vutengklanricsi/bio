8dd38ae9c86e66f2cf76462078da1139d43080e5
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-08 17:49:52 +0100

    adding style to component

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./styles/introduction.css";
+import introPic from "./styles/intro_pic.png";
 
 export default function Introduction() {
   return (
     <div className="introduction-frame">
-      <div className="introduction-text">
-				Hello, welcome to my portfolio page!
-			</div>
+      {/* <hr /> */}
+      <div className="introduction-image">
+        <img src={introPic} alt="intro-pic" />
+      </div>
+      <div className="introduction-title">WELCOME TO MY PORTFOLIO PAGE</div>
     </div>
   );
 }
--- src/page/homePage.js
+++ src/page/homePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar/index";
 import Introduction from "../components/introduction/index";
+import About from "../components/about/index";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <Introduction />
+      <About />
     </>
   );
 }
