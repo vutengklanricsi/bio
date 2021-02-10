1da508482d877d97e00d6936e714ded983014e5d
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-10 19:09:41 +0100

    about component and fill with text

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -6,7 +6,21 @@ export default function About() {
     <>
       <hr />
       <div className="about-frame">
-        <div className="about-frame"></div>
+        <div className="about-title">Vuong-Tan Quang Richard</div>
+        <div className="about-text">
+          I studied faculty of finance and accountancy at Budapest Business
+          School.
+          <br />
+          After two years of study and a half year of internship, I decided to
+          change my career path to become a developer.
+          <br />I have a diploma in web development. Ever since then i
+          self-learned react. My next goal is to learn typescript and writing
+          cleaner codes! My very first was{" "}
+          <a href="https://ebelet.gportal.hu/" target="_blank">
+            website
+          </a>{" "}
+          i created fan teenager.
+        </div>
       </div>
     </>
   );
