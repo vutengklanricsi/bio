a10a2adb51c8b77bb4f66da1154c09fe44629e7d
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-11 14:02:42 +0100

    creating projects const

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -6,25 +6,28 @@ export default function About() {
   return (
     <>
       <hr />
-      <h1>Vuong-Tan Quang Richard</h1>
-      <div className="about-frame">
-        <div className="about-profile-picture">
-          <img src={profilePic} alt="profile" />
-        </div>
-        <div className="about-text">
-          <div className="about-description fadeInLeft">
-            I studied faculty of finance and accountancy at Budapest Business
-            School.
-            <br />
-            After two years of study and a half year of internship, I decided to
-            change my career path to become a developer.
-            <br />I have a diploma in web development.
-            <br /> Ever since then, I self-learned react. My next goal is to
-            learn typescript, improve on my css and writing cleaner codes!For the giggs, this is my very first{" "}
-            <a href="https://ebelet.gportal.hu/" target="_blank">
-              "website"
-            </a>{" "}
-            I created as a teenager.
+      <div className="about-frame-title">
+        <h1>Vuong-Tan Quang Richard</h1>
+        <div className="about-frame">
+          <div className="about-profile-picture">
+            <img src={profilePic} alt="profile" />
+          </div>
+          <div className="about-text">
+            <div className="about-description fadeInLeft">
+              I studied faculty of finance and accountancy at Budapest Business
+              School.
+              <br />
+              After two years of study and a half year of internship, I decided
+              to change my career path to become a developer.
+              <br />I have a diploma in web development.
+              <br /> Ever since then, I self-learned react. My next goal is to
+              learn typescript, improve on my css and writing cleaner codes!For
+              the giggs, this is my very first{" "}
+              <a href="https://ebelet.gportal.hu/" target="_blank">
+                "website"
+              </a>{" "}
+              I created as a teenager.
+            </div>
           </div>
         </div>
       </div>
--- src/projects/index.js
+++ src/projects/index.js
@@ -1,6 +1,23 @@
 export const projects = [
-    {
-        title: 'Mario',
-        
-    }
-]
+  {
+    title: "Portfolio",
+    technology: "Javascript, Html, Css, Git, React",
+  },
+  {
+    title: "Netflix-clone",
+    technology:
+      "Javascript, Html, Css, Git, React, Firebase, Styled Components, FuseJs",
+  },
+  {
+    title: "Mario",
+    technology: "Javascript, Html, Css, Git",
+  },
+  {
+    title: "Tetris",
+    technology: "Javascript, Html, Css, Git",
+  },
+  {
+    title: "Hangman",
+    technology: "Javascript, Html, Css, Git",
+  },
+];
