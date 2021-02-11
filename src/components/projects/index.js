2a64d4d8850f6d79cb9836f2f7507efd5593b2db
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-11 18:45:40 +0100

    creating projects component and add pics

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./styles/projects.css";
 import { projectsConst } from "../../projects/index";
+import ff from "./pictures/3.jpg";
 
 export default function Projects() {
-  // console.log(projectsConst)
+  console.log(ff);
   return (
     <div className="projects-frame">
-      <div className="projects-title">Projects</div>
+      <div className="projects-title">
+        <h1>Projects</h1>
+        190 hagyma 180 repa
+      </div>
       {projectsConst.map((project) => {
         return (
-          <>
+          <div className="projects-item" key={project.id}>
+            <div className="projects-image">
+              <img src={project.picUrl} alt={project.name} />
+            </div>
             <div className="projects-name">{project.name}</div>
-          </>
+          </div>
         );
       })}
     </div>
--- src/projects/index.js
+++ src/projects/index.js
@@ -1,23 +1,38 @@
 export const projectsConst = [
   {
+    id: 1,
     name: "Portfolio",
     technology: "Javascript, Html, Css, Git, React",
+    picUrl: "/static/media/1.32b4c9dc.jpg",
+    linkTo: "https://github.com/vutengklanricsi/bio",
   },
   {
+    id: 2,
     name: "Netflix-clone",
     technology:
       "Javascript, Html, Css, Git, React, Firebase, Styled Components, FuseJs",
+    picUrl: "/static/media/2.73fff4a9.jpg",
+    linkTo: "https://github.com/vutengklanricsi/netflix_clone",
   },
   {
+    id: 3,
     name: "Mario",
     technology: "Javascript, Html, Css, Git",
+    picUrl: "/static/media/3.dd93a260.jpg",
+    linkTo: "https://github.com/fst02/mario",
   },
   {
+    id: 4,
     name: "Tetris",
     technology: "Javascript, Html, Css, Git",
+    picUrl: "/static/media/4.fcce59ed.jpg",
+    linkTo: "https://github.com/fst02/tetris",
   },
   {
+    id: 5,
     name: "Hangman",
     technology: "Javascript, Html, Css, Git",
+    picUrl: "/static/media/5.8d884c1a.jpg",
+    linkTo: "https://github.com/fst02/hangman",
   },
 ];
