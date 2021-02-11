6c1be9270542267e58dca7490f213ffad95c0ffe
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-11 11:42:32 +0100

    WIP making about responsive

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,29 +1,28 @@
 import React from "react";
-import profilePic from "./styles/ricsi_profile.jpg"
+import profilePic from "./styles/ricsi_profile.jpg";
 import "./styles/about.css";
 
 export default function About() {
   return (
     <>
       <hr />
-      <h1>About Me</h1>
+      <h1>Vuong-Tan Quang Richard</h1>
       <div className="about-frame">
         <div className="about-profile-picture">
           <img src={profilePic} alt="profile" />
         </div>
         <div className="about-text">
-          <div className="about-name">Vuong-Tan Quang Richard</div>
           <div className="about-description">
             I studied faculty of finance and accountancy at Budapest Business
             School.
             <br />
             After two years of study and a half year of internship, I decided to
             change my career path to become a developer.
-            <br />I have a diploma in web development. Ever since then i
-            self-learned react. My next goal is to learn typescript and writing
-            cleaner codes! My very first{" "}
+            <br />I have a diploma in web development.
+            <br /> Ever since then, I self-learned react. My next goal is to
+            learn typescript, improve on my css and writing cleaner codes!For the giggs, this my very first{" "}
             <a href="https://ebelet.gportal.hu/" target="_blank">
-              website
+              "website"
             </a>{" "}
             I created as a teenager.
           </div>
