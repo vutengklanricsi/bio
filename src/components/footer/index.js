420d35725c9a76d893582ecb612e834c5b3ec2df
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-25 11:03:43 +0100

    adding footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer-frame">
+      <div className="footer-text">
+        © 2021 Vuong-Tan Quang Richard All Rights Reserved
+      </div>
+    </div>
+  );
+}
--- src/page/homePage.js
+++ src/page/homePage.js
@@ -3,6 +3,7 @@ import Introduction from "../components/introduction/index";
 import About from "../components/about/index";
 import Skills from "../components/skills/index";
 import Projects from "../components/projects/index";
+import Footer from "../components/footer/index";
 
 export default function HomePage() {
   return (
@@ -11,6 +12,7 @@ export default function HomePage() {
       <Skills />
       <About />
       <Projects />
+      <Footer />
     </>
   );
 }
