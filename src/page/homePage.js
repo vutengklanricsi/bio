7df2be063a75178b067fd19b368445ed042c2ba7
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-25 10:35:18 +0100

    small fixes

--- src/page/homePage.js
+++ src/page/homePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/navbar/index";
 import Introduction from "../components/introduction/index";
 import About from "../components/about/index";
 import Skills from "../components/skills/index";
