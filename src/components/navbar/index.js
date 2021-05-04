66396519bf5d3a9837ce624be4002abd4f4f2de1
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-05-04 15:09:25 +0200

    navbar completed(UGLY SOLUTION MUST A REFACTORgit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .)

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -20,92 +20,94 @@ function Navbar() {
 
   return (
     <>
-      <div className="navbar-hamburger">
-        <div
-          id="nav-icon3"
-          className={`${menu ? null : "open"}`}
-          onClick={() => setMenu(!menu)}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+      <div className="navbar-frame">
+        <div className="navbar-hamburger">
+          <div
+            id="nav-icon3"
+            className={`${menu ? null : "open"}`}
+            onClick={() => setMenu(!menu)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
+        {windowWidth > 900 ? (
+          <div className="navbar-window">
+            <a className="navbar-options" href="#">
+              Introduction
+            </a>
+            <a className="navbar-options" href="#about">
+              About Me
+            </a>
+            <a className="navbar-options" href="#skills">
+              Skills
+            </a>
+            <a className="navbar-options" href="#projects">
+              Projects
+            </a>
+            <div className="navbar-logos">
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://github.com/vutengklanricsi"
+              >
+                <img
+                  className="navbar-logo"
+                  src={github}
+                  alt="github"
+                  target="_blank"
+                />
+              </a>
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
+              >
+                <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
+              </a>
+            </div>
+          </div>
+        ) : null}
+        {!menu && windowWidth <= 900 ? (
+          <div className="navbar-window">
+            <a className="navbar-options" href="#">
+              Introduction
+            </a>
+            <a className="navbar-options" href="#about">
+              About Me
+            </a>
+            <a className="navbar-options" href="#skills">
+              Skills
+            </a>
+            <a className="navbar-options" href="#projects">
+              Projects
+            </a>
+            <div className="navbar-logos">
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://github.com/vutengklanricsi"
+              >
+                <img
+                  className="navbar-logo"
+                  src={github}
+                  alt="github"
+                  target="_blank"
+                />
+              </a>
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
+              >
+                <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
+              </a>
+            </div>
+          </div>
+        ) : null}
       </div>
-      {windowWidth > 900 ? (
-        <div className="navbar-frame">
-          <a className="navbar-options" href="#">
-            Introduction
-          </a>
-          <a className="navbar-options" href="#about">
-            About Me
-          </a>
-          <a className="navbar-options" href="#skills">
-            Skills
-          </a>
-          <a className="navbar-options" href="#projects">
-            Projects
-          </a>
-          <div className="navbar-logos">
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://github.com/vutengklanricsi"
-            >
-              <img
-                className="navbar-logo"
-                src={github}
-                alt="github"
-                target="_blank"
-              />
-            </a>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
-            >
-              <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
-            </a>
-          </div>
-        </div>
-      ) : null}
-      {!menu && windowWidth <= 900 ? (
-        <div className="navbar-frame">
-          <a className="navbar-options" href="#">
-            Introduction
-          </a>
-          <a className="navbar-options" href="#about">
-            About Me
-          </a>
-          <a className="navbar-options" href="#skills">
-            Skills
-          </a>
-          <a className="navbar-options" href="#projects">
-            Projects
-          </a>
-          <div className="navbar-logos">
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://github.com/vutengklanricsi"
-            >
-              <img
-                className="navbar-logo"
-                src={github}
-                alt="github"
-                target="_blank"
-              />
-            </a>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
-            >
-              <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
-            </a>
-          </div>
-        </div>
-      ) : null}
       <hr />
     </>
   );
