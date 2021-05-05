d3eef190fd9ffc3e5d7e4c477ed266b82a17b74c
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-05-05 14:58:30 +0200

    small fix in about, changing favicon and title

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -20,14 +20,14 @@ export default function About() {
           </div>
           <div className="about-text">
             <div className="about-description fadeInLeft">
-              I studied faculty of finance and accountancy at Budapest Business
+              I studied finance and accountancy at Budapest Business
               School.
               <br />
-              After two years of study and a half year of internship, I decided
+              After two years of studying finance and a half year of internship, I decided
               to change my career path to become a developer.
               <br />I have a diploma in web development in 2020.
-              <br /> Ever since then, I self-learned react. My next goal is to
-              learn typescript, improve on my css and writing cleaner codes!For
+              <br /> Ever since then, I self-learned different programming languages, like React. My next goal is to
+              learn how to write test automatization, improve on my css and writing cleaner codes!For
               the giggs, this is my very first{" "}
               <a rel="noreferrer" href="https://ebelet.gportal.hu/" target="_blank">
                 "website"
