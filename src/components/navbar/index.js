daac3a0a88da9566361f793caa1cc180fcbceb8e
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-07-11 11:43:12 +0200

    refactoring hamburger menu

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./styles/navbar.css";
 import github from "./styles/github.png";
 import linkedIn from "./styles/linkedIn.png";
 
 function Navbar() {
-  const [menu, setMenu] = useState(true);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  function handleResize() {
-    setWindowWidth(window.innerWidth);
-  }
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [windowWidth]);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const [menu, setMenu] = useState(false);
 
   return (
     <>
@@ -29,7 +13,7 @@ function Navbar() {
         <div className="navbar-hamburger">
           <div
             id="nav-icon3"
-            className={`${menu ? null : "open"}`}
+            className={`${menu ? "open" : null}`}
             onClick={() => setMenu(!menu)}
           >
             <span></span>
@@ -38,86 +22,44 @@ function Navbar() {
             <span></span>
           </div>
         </div>
-        {windowWidth > 900 ? (
-          <div className="navbar-window">
-            <a className="navbar-options" href="#introduction">
-              Introduction
-            </a>
-            <a className="navbar-options" href="#about">
-              About Me
-            </a>
-            <a className="navbar-options" href="#skills">
-              Skills
-            </a>
-            <a className="navbar-options" href="#projects">
-              Projects
-            </a>
-            <a className="navbar-options" href="#contact">
-              Contact
-            </a>
-            <div className="navbar-logos">
-              <a
-                rel="noreferrer"
+        <div className={`navbar-window ${menu ? "active" : null}`}>
+          <a className="navbar-options" href="#introduction">
+            Introduction
+          </a>
+          <a className="navbar-options" href="#about">
+            About Me
+          </a>
+          <a className="navbar-options" href="#skills">
+            Skills
+          </a>
+          <a className="navbar-options" href="#projects">
+            Projects
+          </a>
+          <a className="navbar-options" href="#contact">
+            Contact
+          </a>
+          <div className="navbar-logos">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://github.com/vutengklanricsi"
+            >
+              <img
+                className="navbar-logo"
+                src={github}
+                alt="github"
                 target="_blank"
-                href="https://github.com/vutengklanricsi"
-              >
-                <img
-                  className="navbar-logo"
-                  src={github}
-                  alt="github"
-                  target="_blank"
-                />
-              </a>
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
-              >
-                <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
-              </a>
-            </div>
+              />
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
+            >
+              <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
+            </a>
           </div>
-        ) : null}
-        {!menu && windowWidth <= 900 ? (
-          <div className="navbar-window">
-            <a className="navbar-options" href="#introduction">
-              Introduction
-            </a>
-            <a className="navbar-options" href="#about">
-              About Me
-            </a>
-            <a className="navbar-options" href="#skills">
-              Skills
-            </a>
-            <a className="navbar-options" href="#projects">
-              Projects
-            </a>
-            <a className="navbar-options" href="#contact">
-              Contact
-            </a>
-            <div className="navbar-logos">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://github.com/vutengklanricsi"
-              >
-                <img
-                  className="navbar-logo"
-                  src={github}
-                  alt="github"
-                  target="_blank"
-                />
-              </a>
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://www.linkedin.com/in/richard-vuong-tan-quang-92875917a/"
-              >
-                <img className="navbar-logo" src={linkedIn} alt="linkedIn" />
-              </a>
-            </div>
-          </div>
-        ) : null}
+        </div>
       </div>
     </>
   );
